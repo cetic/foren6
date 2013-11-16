d6da473d82db182f732e25b58fb9bd24cc00aa39
Sébastien Dawans <sebastien.dawans@cetic.be>
2013-11-16 02:40:14 +0100

    Continue until end of file when postprocessing pre

--- textile2html.py
+++ textile2html.py
@@ -28,7 +28,9 @@ def format_pre(text):
         def fn(match):
             return format_pre_rules[match.group()]
         newtext = re.sub('|'.join(re.escape(k) for k in format_pre_rules), fn, text[pre_start:pre_end])
-        text = text[:pre_start] + newtext
+        text = text[:pre_start] + newtext + text[pre_end:]
+        pre_end = pre_start + len(newtext)
+        print(text)
 
 def postprocessing(text):
     text = format_pre(text)
